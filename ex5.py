67ba4a969748d2a5283a454260d14be18271e255
lin <linhangyouxiang@outlook.com>
2016-10-09 21:53:18 -0400

    study drill

--- ex5.py
+++ ex5.py
@@ -1,15 +1,19 @@
 my_name='Zed A. Shaw'
 
 my_age= 35 # is a lie
-my_height=74 # inches
-my_weight = 180 #lbs  not me
+my_height_inches=74 # inches
+my_weight_lbs = 180 #lbs  not me
 my_eyes = 'Blue' # i like my brown
 my_teeth='White'
 my_hair = 'Brown'
+my_height = my_height_inches*2.54
+my_weight=my_weight_lbs*0.454
+
+
 
 print "Let's talk about %s." % my_name
-print "He's %d inches tall." %my_height
-print "He's %d pounds heavy." %my_weight
+print "He's %d cm tall." %my_height
+print "He's %d kg heavy." %my_weight
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." %my_teeth
