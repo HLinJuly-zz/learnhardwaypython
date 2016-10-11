f3791a106aacbba3753627f30c86e908e8a04e6b
lin <linhangyouxiang@outlook.com>
2016-10-11 19:41:58 -0400

    usage of ,

--- ex11.py
+++ ex11.py
@@ -9,3 +9,6 @@ print "How much do you weigh?",
 weight = raw_input()
 
 print "So, you're %r old, %r tall and %r heavy." %(age, height, weight)
+
+
+# We put a , (comma) at the end of each print line. This is so print doesn't end the line with a newline character and go to the next line.
